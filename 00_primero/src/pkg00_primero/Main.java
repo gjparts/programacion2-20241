cd20c076e948a25848f6bb90baa8ede1d6be9a3c
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2024-01-24 12:02:33 -0600

    commit 2401202423214

---
+++ 00_primero/src/pkg00_primero/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg00_primero;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola UNAH");
+        System.out.println("Ingenieria en Sistemas");
+        System.out.print("Me llamo Gerardo");
+        System.out.print("Vivo en San Pedro Sula\nMe gustan las baleadas");
+        System.out.print("\n\n\n\n\n\n");
+        System.out.println("Pera\tManzana\tUva\t\tSandia");
+    }
+    
+}
