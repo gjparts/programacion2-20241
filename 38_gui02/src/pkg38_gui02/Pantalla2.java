99c3e8fffb2694fab7f61cebdc20b930fc0e9a3d
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2024-03-19 10:53:01 -0600

    commit 1903202428356

--- 38_gui02/src/pkg38_gui02/Pantalla2.java
+++ 38_gui02/src/pkg38_gui02/Pantalla2.java
@@ -12,6 +12,7 @@ import java.awt.Color;
  *
  * @author Gerardo Portillo
  */
+import javax.swing.JOptionPane;
 public class Pantalla2 extends javax.swing.JFrame {
 
     /** Creates new form Pantalla2 */
@@ -28,6 +29,14 @@ public class Pantalla2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        caja1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        caja2 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        total = new javax.swing.JTextField();
+        botonSumar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Gerardo 20012002049");
         setBackground(new java.awt.Color(204, 255, 204));
@@ -38,15 +47,79 @@ public class Pantalla2 extends javax.swing.JFrame {
             }
         });
 
+        caja1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        caja1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        caja1.setText("0.00");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Cifra 1:");
+
+        caja2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        caja2.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        caja2.setText("0.00");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setText("Cifra 2:");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel3.setText("Total:");
+
+        total.setEditable(false);
+        total.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        total.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        total.setText("0.00");
+
+        botonSumar.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        botonSumar.setText("Sumar");
+        botonSumar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonSumarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(caja1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(total)
+                            .addComponent(caja2, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE))))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(botonSumar, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(84, 84, 84))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(caja1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(caja2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(total, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addComponent(botonSumar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -56,6 +129,31 @@ public class Pantalla2 extends javax.swing.JFrame {
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         this.getContentPane().setBackground( this.getBackground() );
     }//GEN-LAST:event_formWindowOpened
+
+    private void botonSumarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonSumarActionPerformed
+        //sumar caja1 con caja2
+        //extraer el contenido de los JTextField antes de operarlos
+        
+        //bloque de caza de errores
+        try{
+            //codigo donde se puede generar alguna excepcion
+            double a,b,c;
+            a = Double.parseDouble( caja1.getText() );
+            b = Double.parseDouble( caja2.getText() );
+            //sumar ambos valores
+            c = a+b;
+            //colocar el valor de c en el JTextField llamado total
+            total.setText( String.valueOf(c) );
+        }
+        catch(Exception ex){
+            //codigo a ejecutar en caso de ocurrir una excepcion
+            JOptionPane.showMessageDialog(this, "Alguna cifra no es correcta.");
+            total.setText("ERROR");
+            //JOptionPane.showMessageDialog(this, ex.toString());
+        }
+        
+        
+    }//GEN-LAST:event_botonSumarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -93,6 +191,13 @@ public class Pantalla2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonSumar;
+    private javax.swing.JTextField caja1;
+    private javax.swing.JTextField caja2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField total;
     // End of variables declaration//GEN-END:variables
 
 }
